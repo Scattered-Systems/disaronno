56ed377190ab82ea839ab15f2e4a0c1d89417174
FL03 <jo3mccain@icloud.com>
2022-06-12 11:48:52 +0000

    Update

--- code/crates/disaronno/src/networking/peers.rs
+++ code/crates/disaronno/src/networking/peers.rs
@@ -1,38 +1,28 @@
-use libp2p::{
-    noise::{AuthenticKeypair, Keypair, X25519Spec},
-    PeerId,
-};
+use libp2p;
 
-#[derive(Clone)]
+pub mod types {
+    use libp2p;
+
+    pub type AuthNoiseKey = libp2p::noise::AuthenticKeypair<CryptoSpec>;
+    pub type CryptoSpec = libp2p::noise::X25519Spec;
+    pub type NoiseKey = libp2p::noise::Keypair<CryptoSpec>;
+    pub type PeerId = libp2p::PeerId;
+    pub type PeerKey = libp2p::identity::Keypair;
+}
+
+
+#[derive(Clone, Debug)]
 pub struct Peer {
-    pub id: PeerId,
-    pub key: Keypair,
-    pub noise: AuthenticKeypair<X25519Spec>,
+    pub id: types::PeerId,
+    pub key: types::PeerKey
 }
 
 impl Peer {
-    pub fn new() -> Result<Self, libp2p::noise::NoiseError> {
-        let key = Keychain::generate_ed25519();
-        let noise = Keypair::<X25519Spec>::new()
-            .into_authentic(&key)
-            .expect("Signing libp2p-noise static DH keypair failed.");
-        Ok(
-            Self {
-                id: PeerId::from(key.public()),
-                key: key.clone(),
-                noise: noise.clone(),
-            }
-        )
-    }
-
-    pub fn from(key: Keychain) -> Self {
-        let noise = Keypair::<X25519Spec>::new()
-            .into_authentic(&key)
-            .expect("Signing libp2p-noise static DH keypair failed.");
+    pub fn new() -> Self {
+        let key = types::PeerKey::generate_ed25519();
         Self {
-            id: PeerId::from(key.public()),
-            key: key.clone(),
-            noise: noise.clone(),
+            id: types::PeerId::from(key.public().clone()),
+            key: key.clone()
         }
     }
 }
